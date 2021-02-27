e1d6aa731134f23112d2347eb71d0e00ec303502
zethdeluna <zethdeluna@users.noreply.github.com>
2021-02-27 09:41:31 -0800

    adjusted menu

--- src/MenuMobile.js
+++ src/MenuMobile.js
@@ -4,7 +4,7 @@ import BonBonMenuMobile from './images/BonBonMenuMobile.jpg';
 function MenuMobile() {
     return (
         <div id="menuMobile">
-            <div className="spacer"/>
+            <div class="spacer"><span>Menu</span></div>
             <img src={BonBonMenuMobile} alt="Bon Bon Tea House Menu"/>
         </div>
     )
