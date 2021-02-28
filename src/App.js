09be7bb687cd71e4dc4ad2f4a1b074c0722829f9
zethdeluna <zethdeluna@users.noreply.github.com>
2021-02-28 11:32:25 -0800

    removed custom cursor

--- src/App.js
+++ src/App.js
@@ -31,42 +31,42 @@ class App extends Component {
     // window size
     this.updateDimensions();
     window.addEventListener("resize", this.updateDimensions);
-    // cursor
-    document.addEventListener('mousemove', this.onMouseMove);
-    this.moveCursor();
+    // // cursor
+    // document.addEventListener('mousemove', this.onMouseMove);
+    // this.moveCursor();
   }
   componentWillUnmount() {
     // window size
     window.removeEventListener("resize", this.updateDimensions);
-    // cursor
-    document.removeEventListener('mousemove', this.onMouseMove);
-    cancelAnimationFrame(this.animationFrame);
+    // // cursor
+    // document.removeEventListener('mousemove', this.onMouseMove);
+    // cancelAnimationFrame(this.animationFrame);
   }
-  onMouseMove = (e) => {
-    const { clientX, clientY } = e;
-    this.setState({
-      xMain: clientX,
-      yMain: clientY,
-    });
-  }
-  moveCursor = () => {
-    const { xMain, yMain, xTrailing, yTrailing } = this.state;
-    const xDiff = xMain - xTrailing;
-    const yDiff = yMain - yTrailing;
-    // number in expression is coefficient of the delay
-    this.setState({
-      xTrailing: xTrailing + xDiff / 7,
-      yTrailing: yTrailing + yDiff / 7,
-    },
-    () => {
-      // using refs and transform for better performance
-      if (this.cursor.current) {
-        this.cursor.current.style.transform = `translate3d(${xMain}px, ${yMain}px, 0) translate(-50%, -50%)`;
-        this.cursorTrailing.current.style.transform = `translate3d(${xTrailing}px, ${yTrailing}px, 0) translate(-50%, -50%)`;
-        this.animationFrame = requestAnimationFrame(this.moveCursor);
-      }
-    });
-  }
+  // onMouseMove = (e) => {
+  //   const { clientX, clientY } = e;
+  //   this.setState({
+  //     xMain: clientX,
+  //     yMain: clientY,
+  //   });
+  // }
+  // moveCursor = () => {
+  //   const { xMain, yMain, xTrailing, yTrailing } = this.state;
+  //   const xDiff = xMain - xTrailing;
+  //   const yDiff = yMain - yTrailing;
+  //   // number in expression is coefficient of the delay
+  //   this.setState({
+  //     xTrailing: xTrailing + xDiff / 7,
+  //     yTrailing: yTrailing + yDiff / 7,
+  //   },
+  //   () => {
+  //     // using refs and transform for better performance
+  //     if (this.cursor.current) {
+  //       this.cursor.current.style.transform = `translate3d(${xMain}px, ${yMain}px, 0) translate(-50%, -50%)`;
+  //       this.cursorTrailing.current.style.transform = `translate3d(${xTrailing}px, ${yTrailing}px, 0) translate(-50%, -50%)`;
+  //       this.animationFrame = requestAnimationFrame(this.moveCursor);
+  //     }
+  //   });
+  // }
 
   // track window size and update state
   updateDimensions () {
@@ -80,7 +80,7 @@ class App extends Component {
     const width = this.state.width;
     let navigation, imageGallery, menu, locations;
 
-    if (width < 780) {
+    if (width < 800) {
       navigation = <div><NavigationMobile/></div>;
       imageGallery = <div><ImageGalleryMobile/></div>
       menu = (
@@ -91,33 +91,33 @@ class App extends Component {
       );
       locations = <div><LocationsMobile/></div>
     } else {
-      // cursor effect on hover over links
-      const links = document.querySelectorAll('a,button');
+      // // cursor effect on hover over links
+      // const links = document.querySelectorAll('a,button');
 
-      links.forEach(link => {
-        link.addEventListener('mouseenter', e => {
-          this.cursorTrailing.current.classList.add('enlarged');
-        })
-        link.addEventListener('mouseout', e => {
-          this.cursorTrailing.current.classList.remove('enlarged')
-        })
-      })
+      // links.forEach(link => {
+      //   link.addEventListener('mouseenter', e => {
+      //     this.cursorTrailing.current.classList.add('enlarged');
+      //   })
+      //   link.addEventListener('mouseout', e => {
+      //     this.cursorTrailing.current.classList.remove('enlarged')
+      //   })
+      // })
 
-      window.onmousedown = () => {
-        this.cursor.current.classList.add('clicked');
-      }
-      window.onmouseup = () => {
-        this.cursor.current.classList.remove('clicked');
-      }
+      // window.onmousedown = () => {
+      //   this.cursor.current.classList.add('clicked');
+      // }
+      // window.onmouseup = () => {
+      //   this.cursor.current.classList.remove('clicked');
+      // }
 
       navigation = <div><Navigation/></div>;
 
       imageGallery = (
         <div>
-          <div className="cursors">
+          {/* <div className="cursors">
             <div className="cursor fade-in" ref={this.cursor}/>
             <div className="cursor fade-in" ref={this.cursorTrailing}/>
-          </div>
+          </div> */}
           <ImageGallery/>
         </div>
       );
