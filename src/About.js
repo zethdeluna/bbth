e4fe68c64fa678781a1c3f901087806286e33b16
zethdeluna <zethdeluna@users.noreply.github.com>
2021-03-01 09:00:13 -0800

    created About sections

---
+++ src/About.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+function About() {
+    return (
+        <div id="aboutPage">
+            <div id="title">
+                <h2>About</h2>
+            </div>
+            <div id="story">
+                <p>
+                    <em>DISCLAIMER: 
+                    <br/>
+                    This is not the official website for Bon Bon Tea House, 
+                    <br/>
+                    just a little project created by one of their workers who is beginning their journey as a web developer.</em>
+                    <br/><br/>
+                    What is my perfect crime?
+                    <br/><br/>
+                    I break into Bon Bon Tea House at midnight. Do I go for the vault? No, I go for the boba. It's priceless.
+                    As I'm scooping it up, a woman catches me. She tells me to stop. It's her business. She's Ms. Tea House.
+                    I say no. We have milk teas all night. In the morning, the cops come and I escape in one of their uniforms.
+                    I tell her to meet me in Mexico, but I go to Canada. I don't trust her. Besides, I like the cold.
+                    Thirty years later, I get a postcard. I have a son; and he's the chief of police. This is where the story
+                    gets interesting. I tell Ms. Tea House to meet me in Paris by the Trocadero. She's been waiting for me all 
+                    these years. She's never had another milk tea. I don't care. I don't show up. I go to Berlin. 
+                    That's were I stashed the boba.
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default About
---
+++ src/AboutMobile.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+function AboutMobile() {
+    return (
+        <div id="aboutMobile">
+            <div id="titleMobile">
+                <h2>About</h2>
+            </div>
+            <div id="storyMobile">
+                <p>
+                    <em>DISCLAIMER: 
+                    <br/>
+                    This is not the official website for Bon Bon Tea House, 
+                    just a little project created by one of their team members who is beginning their journey as a web developer.</em>
+                    <br/><br/>
+                    What is my perfect crime?
+                    <br/><br/>
+                    I break into Bon Bon Tea House at midnight. Do I go for the vault? No, I go for the boba. It's priceless.
+                    As I'm scooping it up, a woman catches me. She tells me to stop. It's her business. She's Ms. Tea House.
+                    I say no. We have milk teas all night. In the morning, the cops come and I escape in one of their uniforms.
+                    I tell her to meet me in Mexico, but I go to Canada. I don't trust her. Besides, I like the cold.
+                    Thirty years later, I get a postcard. I have a son; and he's the chief of police. This is where the story
+                    gets interesting. I tell Ms. Tea House to meet me in Paris by the Trocadero. She's been waiting for me all 
+                    these years. She's never had another milk tea. I don't care. I don't show up. I go to Berlin. 
+                    That's were I stashed the boba.
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default AboutMobile
